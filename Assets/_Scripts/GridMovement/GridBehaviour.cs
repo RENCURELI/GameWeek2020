013454fd94a48b6e8a5eebd9283e2b39c305c346
RENCURELI <pierre.rencurel@orange.fr>
2020-04-15 09:17:27 -0400

    First grid generation
    DONE : Generate basic grid with prefabs
    TODO : Add aditional logic for player movement on grid

--- Assets/_Scripts/GridMovement/GridBehaviour.cs
+++ Assets/_Scripts/GridMovement/GridBehaviour.cs
@@ -4,6 +4,42 @@ using UnityEngine;
 
 public class GridBehaviour : MonoBehaviour
 {
+
+    #region Variables
+    /// <summary>
+    /// Used to define number of rows on grid
+    /// </summary>
+    [Header("Number of rows and columns")]
+    public int rows = 10;
+    /// <summary>
+    /// Used to define number of columns on grid
+    /// </summary>
+    public int columns = 10;
+
+    /// <summary>
+    /// Number of units between each point
+    /// </summary>
+    [Header("Ditance between points")]
+    public int scale = 5;
+
+    [Header("Grid Point Prefab")]
+    public GameObject gridPrefab;
+
+    /// <summary>
+    /// Start point of grid generation
+    /// </summary>
+    public Vector3 generationStart = new Vector3(0, 0, 0);
+
+    #endregion
+
+    private void Awake()
+    {
+        if (gridPrefab)
+            GenerateGrid();
+        else
+            print("Prefab missing");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +51,23 @@ public class GridBehaviour : MonoBehaviour
     {
         
     }
+
+    #region Functions
+
+    void GenerateGrid()
+    {
+        for (int i = 0; i < columns; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                GameObject instance = Instantiate(gridPrefab, new Vector3(generationStart.x + scale * i, generationStart.y, generationStart.z + scale * j), Quaternion.identity);
+                instance.transform.SetParent(gameObject.transform);
+                instance.GetComponent<GridStat>().x = i;
+                instance.GetComponent<GridStat>().y = j;
+            }
+        }
+    }
+
+
+    #endregion
 }
--- Assets/_Scripts/GridMovement/GridStat.cs
+++ Assets/_Scripts/GridMovement/GridStat.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GridStat : MonoBehaviour
 {
-    public int visited = 1;
+    public int visited = -1;
     public int x = 0;
     public int y = 0;
 
